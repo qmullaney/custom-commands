e788f636a6dec9b7ee564a59a1dbed6749797a58
Quinn Mullaney <skiingqm@gmail.com>
2022-07-22 10:34:09 -0500

    display check

--- repeating_checklist.rb
+++ repeating_checklist.rb
@@ -1,6 +1,4 @@
 class Checklist
-    @@tasks = []
-
     def self.display
         file = File.open("list.txt")
         file_data = file.readlines.map(&:chomp)
@@ -8,14 +6,19 @@ class Checklist
         iterator = 1
         file_data.each do |line|
             if line.index("-&-") != 0
-                puts "#{interator} #{line}"
-                interator += 1
+                puts "#{iterator} #{line}"
+                iterator += 1
             end
         end
         file.close
     end
-
-    def self.setup
-        File.open("list.txt", [a]) {|f| f.write("wake up") }
-    end 
 end
+
+def main(arg)
+    case arg
+    when "display"
+        Checklist.display
+    end
+end
+
+mains
